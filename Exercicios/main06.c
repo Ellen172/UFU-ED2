82301254c54613edb80ae7e93ba5663f9ee74f3e
EllenCas172 <ellencas172@gmail.com>
2021-07-30 22:01:25 -0300

    resolução ex 6

--- Exercicios/main06.c
+++ Exercicios/main06.c
@@ -1,18 +1,38 @@
 #include <stdio.h>
+#include <stdlib.h>
+
+int somaMoedas(int qtd, int *moedas, int i, int d, int maior) {
+    int j=i, soma=0;
+    if(i >= qtd){
+        return maior;
+    }
+    while(qtd>0 && j<qtd){
+        soma += moedas[j];
+        j+=d; 
+    }
+    if(soma > maior){
+        maior = soma;
+    } 
+    if(d >= qtd){
+        return somaMoedas(qtd, moedas, i+1, 2, maior);
+    } 
+    return somaMoedas(qtd, moedas, i, d+1, maior);
+
+}
 
 int main(){
     int teste;
     scanf("%d", &teste);
     for(int i=0; i<teste; i++){
-        int qtd;
+        int qtd, distancia=2, inicio=0;
         scanf("%d", &qtd);
-        int moedas, pares=0, impares=0;
+        int moedas[qtd];
         for(int j=0; j<qtd; j++){
-            scanf("%d", &moedas);
-            if(j%2==0) pares+=moedas;
-            else impares+=moedas;
+            scanf("%d", &moedas[j]);
         }
-        if(pares > impares) printf("Caso %d: %d\n", i+1, pares);
-        else printf("Caso %d: %d\n", i+1, impares);
+        int maior = 0;
+        int resposta = somaMoedas(qtd, moedas, inicio, distancia, maior);
+        printf("Caso %d: %d\n", i+1, resposta);
     }
+    return 0;
 }
