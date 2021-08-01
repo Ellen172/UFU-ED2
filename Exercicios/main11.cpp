33db6b969ff30058bb5111b4b28f921df093ed24
EllenCas172 <ellencas172@gmail.com>
2021-08-01 12:49:12 -0300

    resolução incompleta

---
+++ Exercicios/main11.cpp
@@ -0,0 +1,81 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+double defineMoeda(double valor){
+    if(valor >= 100) return 100;
+    else if(valor >= 50) return 50;
+    else if(valor >= 20) return 20;
+    else if(valor >= 10) return 10;
+    else if(valor >= 5) return 5;
+    else if(valor >= 2) return 2;
+    else if(valor >= 1) return 1;
+    else if(valor >= 0.5) return 0.5;
+    else if(valor >= 0.2) return 0.2;
+    else if(valor >= 0.1) return 0.1;
+    else return 0.05;
+}
+
+double trocaMoeda(double moeda){
+    if(moeda == 100) return 50;
+    if(moeda == 50) return 20;
+    if(moeda == 20) return 10;
+    if(moeda == 10) return 5;
+    if(moeda == 5) return 2;
+    if(moeda == 2) return 1;
+    if(moeda == 1) return 0.5;
+    if(moeda == 0.5) return 0.2;
+    if(moeda == 0.2) return 0.1;
+    if(moeda == 0.1) return 0.05;
+}
+
+int combinacoes(double valor_inicial, double valor, int comb, double moeda, double moeda_inicial){
+    cout << "moeda = " << moeda << endl;
+    cout << "moeda_inicial = " << moeda_inicial << endl;
+    cout << "valor = " << valor << endl;
+    int cont=0;
+    while(valor-moeda >= 0.00){
+        cout << "while" << endl;
+        valor -= moeda;
+        cont++;
+    }
+    cout << cont << "x " << moeda << endl;
+
+    if(cont > 0){
+        comb++;
+        cout << "comb = " << comb << endl;
+    }
+
+    if(moeda == 0.05 && moeda_inicial == 0.05) {
+        cout << "if 1" << endl;
+        return comb;
+    }
+
+    if(valor == 0 && cont > 1){
+        cout << "if 2" << endl;
+        valor = valor_inicial -= moeda;
+        moeda = trocaMoeda(moeda_inicial);
+        return combinacoes(valor_inicial, valor, comb, moeda, moeda);
+    }
+
+    if(valor == 0 && cont == 1) {
+        cout << "if 3" << endl;
+        moeda = trocaMoeda(moeda_inicial);
+        return combinacoes(valor_inicial, valor_inicial, comb, moeda, moeda);
+    }
+
+
+    cout << "else" << endl;
+    return combinacoes(valor_inicial, valor, comb, trocaMoeda(moeda), moeda_inicial);
+}
+
+int main(){
+    double valor; 
+    cin >> valor;
+    while(valor > 0.0){
+        double moeda_inicial = defineMoeda(valor);
+        int comb=0;
+        cout << "resposta = " << combinacoes(valor, valor, comb, moeda_inicial, moeda_inicial) << endl;
+
+        cin >> valor;
+    }
+}
